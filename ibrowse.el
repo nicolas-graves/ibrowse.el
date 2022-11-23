04ce2e9531b0864998509dea567b1bbbf032aa38
Nicolas Graves <ngraves@ngraves.fr>
2022-11-23 14:32:19 +0100

    creating file ibrowse.el

---
+++ ibrowse.el
@@ -0,0 +1,34 @@
+;; ibrowse.el --- Interact with your browser from Emacs -*- lexical-binding: t -*-
+
+;; Copyright © 2022 Nicolas Graves <ngraves@ngraves.fr>
+
+;; Author: Nicolas Graves <ngraves@ngraves.fr>
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.3") (let-alist "1.0.4") (seq "1.11") (dash "2.12.1"))
+;; Keywords: browser, tabs, switch
+;; URL: https://git.sr.ht/~ngraves/ibrowse.el
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Interact with your browser from Emacs
+
+;;; Code:
+
+(require 'ibrowse-tab)
+(require 'ibrowse-bookmark)
+(require 'ibrowse-history)
+
+(provide 'ibrowse)
+;;; ibrowse.el ends here
